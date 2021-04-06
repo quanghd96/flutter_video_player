1bbcd9325b849edeef098e1c09beda3ab5d05754
Dang Ngoc Quang <quanghd96@gmail.com>
2021-04-06 14:16:08 +0700

    add ijk player

--- android/src/main/java/xyz/justsoft/video_thumbnail/VideoThumbnailPlugin.java
+++ android/src/main/java/xyz/justsoft/video_thumbnail/VideoThumbnailPlugin.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.SurfaceTexture;
 import android.net.Uri;
+import android.view.Surface;
 import android.view.TextureView;
 import android.view.ViewGroup;
 
@@ -31,7 +32,7 @@ import java.util.Map;
 
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.view.TextureRegistry;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 public class VideoThumbnailPlugin implements EventChannel.StreamHandler {
     private static final String FORMAT_SS = "ss";
@@ -40,6 +41,8 @@ public class VideoThumbnailPlugin implements EventChannel.StreamHandler {
     private static final String FORMAT_OTHER = "other";
 
     private SimpleExoPlayer exoPlayer;
+    private IjkMediaPlayer ijkPlayer;
+
     private static Registrar mRegistrar;
     private EventChannel.EventSink eventSink;
 
@@ -54,19 +57,34 @@ public class VideoThumbnailPlugin implements EventChannel.StreamHandler {
         eventSink = events;
         Map<String, Object> args = (Map<String, Object>) arguments;
         String video = (String) args.get("video");
-        playVideo(video);
+        if (video.startsWith("rtsp")) {
+            playVideoIjk(video);
+        } else {
+            playVideoExoPlayer(video);
+        }
     }
 
     @Override
     public void onCancel(Object arguments) {
         try {
-            exoPlayer.stop();
-            exoPlayer.release();
+            if (exoPlayer != null) {
+                exoPlayer.stop();
+                exoPlayer.release();
+            }
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            if (ijkPlayer != null) {
+                ijkPlayer.stop();
+                ijkPlayer.release();
+            }
+        } catch (Exception e) {
+
         }
     }
 
-    private void playVideo(String url) {
+    private void playVideoExoPlayer(String url) {
         exoPlayer = new SimpleExoPlayer.Builder(mRegistrar.context()).build();
         Uri uri = Uri.parse(url);
         DataSource.Factory dataSourceFactory =
@@ -82,6 +100,66 @@ public class VideoThumbnailPlugin implements EventChannel.StreamHandler {
         exoPlayer.prepare();
         setupVideoPlayer();
         exoPlayer.setPlayWhenReady(true);
+    }
+
+    private void playVideoIjk(String url) {
+        ijkPlayer = new IjkMediaPlayer();
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "videotoolbox", 0);
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-hevc", 1);
+
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "rtsp_transport", "tcp");
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "rtsp_flags", "prefer_tcp");
+
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "allowed_media_types", "video");
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "timeout", 10 * 1000 * 1000);
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "max-buffer-size", 1024);
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "infbuf", 1);
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzemaxduration", 100L);
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "probesize", 10240L);
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "flush_packets", 1L);
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "packet-buffering", 0L);
+        ijkPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 60L);
+        try {
+            ijkPlayer.setDataSource(url);
+            TextureView textureView = new TextureView(mRegistrar.context());
+            textureView.setAlpha(0);
+            int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+
+            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(width, width);
+            mRegistrar.activity().addContentView(textureView, params);
+            textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+                @Override
+                public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
+                    ijkPlayer.setSurface(new Surface(surfaceTexture));
+                }
+
+                @Override
+                public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
+
+                }
+
+                @Override
+                public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surfaceTexture) {
+                    return false;
+                }
+
+                @Override
+                public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surfaceTexture) {
+                    try {
+                        Bitmap bmp = textureView.getBitmap();
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        bmp.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                        byte[] byteArray = stream.toByteArray();
+                        bmp.recycle();
+                        eventSink.success(byteArray);
+                    } catch (Exception e) {
+                    }
+                }
+            });
+            ijkPlayer.prepareAsync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private MediaSource buildMediaSource(
@@ -132,22 +210,17 @@ public class VideoThumbnailPlugin implements EventChannel.StreamHandler {
     }
 
     private void setupVideoPlayer() {
-        TextureRegistry.SurfaceTextureEntry handle =
-                mRegistrar.textures().createSurfaceTexture();
         TextureView textureView = new TextureView(mRegistrar.context());
         textureView.setAlpha(0);
-        textureView.setSurfaceTexture(handle.surfaceTexture());
         exoPlayer.setVideoTextureView(textureView);
-
         int width = Resources.getSystem().getDisplayMetrics().widthPixels;
-
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(width, width);
         mRegistrar.activity().addContentView(textureView, params);
 
         textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
             @Override
             public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
-
+                exoPlayer.setVideoSurface(new Surface(surfaceTexture));
             }
 
             @Override
